a5ac10ac38339d37619721fc211622eb070fcae7
oxypteros <oxypteros@gmail.com>
2025-12-10 16:16:51 +0000

    Merge pull request #1 from oxypteros/chore/project-init
    Chore/project init

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+import Alpine from '@alpinejs/csp';
+
+window.Alpine = Alpine;
+
+Alpine.start();
---
+++ e2e/home.spec.js
@@ -0,0 +1,29 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('/');
+
+  // Check that Title exists (SEO)
+  await expect(page).toHaveTitle(/\S/);
+});
+
+test('Structure: Every section must have a heading', async ({ page }) => {
+  await page.goto('/');
+
+  // Find all <section> tags
+  const sections = page.locator('section');
+  const count = await sections.count();
+
+  // Prevent false positives
+  expect(count).toBeGreaterThan(0);
+
+  for (let i = 0; i < count; i++) {
+    const section = sections.nth(i);
+
+    // Use .count() instead of .toBeVisible() in case of sr-only headings
+    const headingCount = await section.locator('h1, h2, h3, h4, h5, h6').count();
+
+    expect(headingCount, `Section #${i + 1} is missing a heading!`).toBeGreaterThan(0);
+  }
+});
